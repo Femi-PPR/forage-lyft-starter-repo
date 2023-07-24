97c234f4d04de169b87ec162d7f91ece4c615311
Work-PC <femioladapo3@gmail.com>
2023-07-24 13:06:13 +0100

    refactored car code

---
+++ model/car_factory.py
@@ -0,0 +1,24 @@
+from car import Car
+from engine.capulet_engine import CapuletEngine
+from engine.willoughby_engine import WilloughbyEngine
+from engine.sternman_engine import SternmanEngine
+from battery.nubbin_battery import NubbinBattery
+from battery.spindler_battery import SpindlerBattery
+
+class CarFactory():
+
+	def create_calliope(self, current_date, last_service_date, current_mileage, last_service_mileage):
+		return Car(CapuletEngine(current_mileage, last_service_mileage), SpindlerBattery(current_date, last_service_date))
+
+	def create_glissade(self, current_date, last_service_date, current_mileage, last_service_mileage):
+		return Car(WilloughbyEngine(current_mileage, last_service_mileage), SpindlerBattery(current_date, last_service_date))
+
+	def create_palindrome(self, current_date, last_service_date, warning_light_is_on):
+		return Car(SternmanEngine(current_mileage, last_service_mileage), SpindlerBattery(current_date, last_service_date))
+
+	def create_rorschach(self, current_date, last_service_date, current_mileage, last_service_mileage):
+		return Car(WilloughbyEngine(current_mileage, last_service_mileage), NubbinBattery(current_date, last_service_date))
+
+	def create_thovex(self, current_date, last_service_date, current_mileage, last_service_mileage):
+		return Car(CapuletEngine(current_mileage, last_service_mileage), NubbinBattery(current_date, last_service_date))
+		
